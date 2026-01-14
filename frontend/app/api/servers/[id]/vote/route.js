ab36b814ad09a126053b1c95418f8e3c5f7dcec9
ccursor68-afk <ccursor68@gmail.com>
2026-01-14 20:23:57 +0300

    Update route.js

--- frontend/app/api/servers/[id]/vote/route.js
+++ frontend/app/api/servers/[id]/vote/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
-import { supabaseAdmin } from '../../../../../lib/supabase.js'
+import { supabaseAdmin } from '@/lib/supabase'
+
 
 export async function POST(request, { params }) {
   try {
